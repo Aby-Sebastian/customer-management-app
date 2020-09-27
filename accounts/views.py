de78d8dcc0bb8119b45e1fd48a1db214ce8bf0b5
Aby-Sebastian <abysebastian98@gmail.com>
2020-09-27 21:11:51 +0530

    created new templates

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return render(request, 'accounts/dashboard.html')
+	return render(request, 'accounts/main/dashboard.html')
 
 def products(request):
-	return render(request, 'accounts/products.html')
+	return render(request, 'accounts/products/products.html')
 
 def customer(request):
-	return render(request, 'accounts/customer.html')
+	return render(request, 'accounts/customer/customer.html')
