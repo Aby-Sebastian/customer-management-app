008ea96a46e8e9f3f00feb7dcd4557796ebabb59
Aby-Sebastian <abysebastian98@gmail.com>
2020-10-04 23:56:18 +0530

    all models registered

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,5 +5,5 @@ from .models import Customer, Product, Order, Tag
 
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Order)
+admin.site.register(Order)	
 admin.site.register(Tag)
