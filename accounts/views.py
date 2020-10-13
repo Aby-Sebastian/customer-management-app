d292aa2591814e70bfa9b625b508d399089e7478
Aby-Sebastian <abysebastian98@gmail.com>
2020-10-14 00:04:35 +0530

    added django messages

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 from .filters import OrderFilter
@@ -20,6 +21,8 @@ def registerPage(request):
 		form = CreateUserForm(request.POST)
 		if form.is_valid():
 			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, message='Account was created for ' + user)
 			return redirect('login')
 
 	context={"form":form,'test':'testing'}
