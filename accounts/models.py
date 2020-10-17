d0d60e7f3bb3e9bd059c46ad7c01f09618108726
Aby-Sebastian <abysebastian98@gmail.com>
2020-10-17 22:59:01 +0530

    profile pic added

--- accounts/models.py
+++ accounts/models.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
 	"""docstring for Customer"""
-	user = models.OneToOneField(User, null=True,  on_delete=models.CASCADE)
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=20, null=True)
 	email = models.EmailField(null=True)
+	profile_pic = models.ImageField(default='default.webp',null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True)
 	
 	def __str__(self):
--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 	path('customer/<str:pk>/', views.customer, name='customer'),
 	path('products/', views.products, name='products'),
 
+	path('account/', views.accountSettings, name='account'),
+
 	path('create_order/<str:pk>/', views.createOrder, name='create_order'),
 	path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
 	path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
